dc625309ffc5f39285b415f767b34116dce223e1
jjangarita <jjangaria34@misena.edu.co>
2021-10-15 19:21:28 -0500

    "1"

--- app/src/main/java/com/jjangarita/calc2/MainActivity.kt
+++ app/src/main/java/com/jjangarita/calc2/MainActivity.kt
@@ -10,6 +10,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        //val mToolbar : Toolbar
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
